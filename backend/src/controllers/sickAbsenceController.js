46bb1c79e87abfbc8d4a34a097fec36828ef773f
neratmatevz <matevz.nerat@gmail.com>
2024-11-10 13:29:14 +0100

    setup: api endpoints and controllers

---
+++ backend/src/controllers/sickAbsenceController.js
@@ -0,0 +1,15 @@
+
+exports.getAllSickAbsenceHours = (req, res) => {    
+};
+
+exports.addSickAbsenceHours = (req, res) => {
+   
+};
+
+exports.updateSickAbsenceHours = (req, res) => {
+    const id = req.params.id;
+};
+
+exports.deleteSickAbsenceHours = (req, res) => {
+    const id = req.params.id;
+};
--- backend/src/routes/sickAbsenceRoutes.js
+++ backend/src/routes/sickAbsenceRoutes.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const sickAbsenceController = require('../controllers/sickAbsenceController');
+
+
+router.get('/', sickAbsenceController.getAllSickAbsenceHours);
+router.post('/', sickAbsenceController.addSickAbsenceHours);
 
 
 module.exports = router;
