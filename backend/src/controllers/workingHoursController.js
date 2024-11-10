b635fbc8e93dd56c9aa0a07d6e7ca4ebc4234521
Lea Tajnikar <lea.tajnikar@student.um.si>
2024-11-10 17:31:41 +0100

    - branje delovnih ur - dodajanje delovnih ur - spreminjanje delovnih ur - brisanje delovnih ur

--- backend/src/controllers/workingHoursController.js
+++ backend/src/controllers/workingHoursController.js
@@ -1,24 +1,95 @@
+const { getFirestoreInstance } = require('../config/firebase');
+const admin = require('firebase-admin');
 
 
-exports.getAllHours = (req, res) => {
+exports.getAllHours = async (req, res) => {
     // Logic to retrieve all working hours from the database or data source
+    const userId = req.query.userId;
+    let db = getFirestoreInstance();
+    const userDoc = await db.collection('users').doc(userId).get();
+
+    res.status(200).json({
+        userId: userId,
+        workHours: userDoc.data()["workHours"]
+    });
 };
 
-exports.addHours = (req, res) => {
+exports.addHours = async (req, res) => {
     // Logic to add working hours based on request body data
+
+    const userId = req.query.userId;
+    let db = getFirestoreInstance();
+    const userDoc = await db.collection('users').doc(userId);
+
+    let reqData = req.body;
+    reqData.date = admin.firestore.Timestamp.fromDate(new Date(reqData.date ));
+
+     let data = (await userDoc.get()).data();
+        data["workHours"].push(reqData);
+
+
+    await userDoc.set(data);
+
+    res.status(200).json({
+        userId: userId,
+        workHours: userDoc.data()["workHours"]
+    });
+
 };
 
-exports.getHours = (req, res) => {
+exports.getHours = async (req, res) => {
+    // Logic to retrieve all working hours from the database or data source
+    const userId = req.query.userId;
     const id = req.params.id;
-    // Logic to retrieve working hours for the given ID
+    let db = getFirestoreInstance();
+    const userDoc = await db.collection('users').doc(userId).get();
+
+    res.status(200).json({
+        userId: userId,
+        workHours: userDoc.data()["workHours"][id]
+    });
 };
 
-exports.updateHours = (req, res) => {
+exports.updateHours = async (req, res) => {
     const id = req.params.id;
     // Logic to update working hours for the given ID based on request body data
+
+    const userId = req.query.userId;
+    let db = getFirestoreInstance();
+    const userDoc = await db.collection('users').doc(userId);
+
+    let reqData = req.body;
+    if("date" in reqData) {
+        reqData.date = admin.firestore.Timestamp.fromDate(new Date(reqData.date));
+    }
+    let data = (await userDoc.get()).data();
+
+    data["workHours"][id] = {...data["workHours"][id], ...reqData};
+
+    await userDoc.set(data);
+
+    res.status(200).json({
+        userId: userId,
+        workHours: data["workHours"][id]
+    });
+
 };
 
-exports.deleteHours = (req, res) => {
+exports.deleteHours = async (req, res) => {
     const id = req.params.id;
     // Logic to delete working hours for the given ID
+
+    const userId = req.query.userId;
+    let db = getFirestoreInstance();
+    const userDoc = await db.collection('users').doc(userId);
+
+    let data = (await userDoc.get()).data();
+    data["workHours"].splice(parseInt(id), 1);
+
+    await userDoc.set(data);
+
+    res.status(200).json({
+        userId: userId,
+        workHours: data["workHours"]
+    });
 };
